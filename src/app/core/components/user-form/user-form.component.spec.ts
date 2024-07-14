0ff520779dbbcc6cbd311e1bdf6d4c5b1f0dddef
Mariam <marvellousmariam2006@gmail.com>
2024-07-14 10:12:06 +0100

    i wrote the footer and test a component

--- src/app/core/components/user-form/user-form.component.spec.ts
+++ src/app/core/components/user-form/user-form.component.spec.ts
@@ -1,6 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// user-form.component.spec.ts
 
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UserFormComponent } from './user-form.component';
+import { Component } from '@angular/core';
 
 describe('UserFormComponent', () => {
   let component: UserFormComponent;
@@ -8,10 +11,12 @@ describe('UserFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserFormComponent]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule],
+      declarations: [UserFormComponent]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +24,52 @@ describe('UserFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a form with username, workout type, and minutes fields', () => {
+    const form = fixture.nativeElement.querySelector('form');
+    expect(form).toBeTruthy();
+
+    const usernameInput = form.querySelector('input[id="username"]');
+    expect(usernameInput).toBeTruthy();
+
+    const workoutTypeSelect = form.querySelector('select[id="workout-type"]');
+    expect(workoutTypeSelect).toBeTruthy();
+
+    const minutesInput = form.querySelector('input[id="minutes"]');
+    expect(minutesInput).toBeTruthy();
+  });
+
+  it('should submit the form when the submit button is clicked', () => {
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    expect(submitButton).toBeTruthy();
+
+    const form = fixture.nativeElement.querySelector('form');
+ 
+
+    submitButton.click();
+    expect(Component).toHaveBeenCalledTimes(1);
+  });
+
+  it('should validate the form when the submit button is clicked', () => {
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    expect(submitButton).toBeTruthy();
+
+    const form = fixture.nativeElement.querySelector('form');
+    const usernameInput = form.querySelector('input[id="username"]');
+    const minutesInput = form.querySelector('input[id="minutes"]');
+
+    usernameInput.value = '';
+    minutesInput.value = '';
+
+    submitButton.click();
+
+
+    usernameInput.value = 'John Doe';
+    minutesInput.value = '30';
+
+    submitButton.click();
+
+   
   });
 });
